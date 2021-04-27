99521ec3265d81bf253f09aea9224b977d1c973a
d-i-m-a <ivankin00@list.ru>
2021-04-28 00:18:43 +0700

    функция подсчета занимаемого места по расширениям файлов

--- main.cpp
+++ main.cpp
@@ -9,27 +9,76 @@
 
 QList<std::pair<QString,double>> computeFilesSize(const QString &path)
 {
-    QList<std::pair<QString,double>> filesSize;
+    QList<std::pair<QString,double>> filesSizeList;
 
     if(!path.isEmpty())
     {
         QDir directory(path);
 
         if(!directory.exists())
-            throw QString("Directory does not exists");
+            throw QString("Directory does not exist");
 
-        QFileInfoList filesInfo = directory.entryInfoList();
+        QFileInfoList filesInfoList = directory.entryInfoList();
 
         double dirSize = QFileInfo(path).size();
 
-        for (QFileInfo &curFile : filesInfo)
+        for (QFileInfo &curFile : filesInfoList)
         {
-            filesSize.push_back(std::pair<QString,double> (curFile.fileName(),curFile.size()/dirSize));
+            filesSizeList.push_back(std::pair<QString,double> (curFile.fileName(),curFile.size()/dirSize));
         }
-
     }
 
-    return filesSize;
+    return filesSizeList;
+}
+
+QList<std::pair<QString,double>> computeExtensionsSize(const QString &path)
+{
+    QList<std::pair<QString,double>> extensionsSizeList;
+
+    if(!path.isEmpty())
+    {
+        QDir directory(path);
+
+        if(!directory.exists())
+            throw QString("Directory does not exist");
+
+        QFileInfoList filesInfoList = directory.entryInfoList(QDir::Filter::Files);
+
+        QStringList dirList = directory.entryList(QDir::Filter::Dirs);
+
+        for(QString &curDir : dirList)
+        {
+            filesInfoList += QDir(curDir).entryInfoList(QDir::Filter::Files);
+            dirList += QDir(curDir).entryList(QDir::Filter::Dirs);
+        }
+
+        double dirSize = QFileInfo(path).size();
+
+        QFileInfoList::iterator iterFileInfo;
+        QString curSuffix;
+
+        while(!filesInfoList.isEmpty())
+        {
+            curSuffix = filesInfoList.front().suffix();
+            extensionsSizeList.append(std::pair<QString,double>(curSuffix,0));
+
+            iterFileInfo = filesInfoList.begin();
+
+            while (iterFileInfo != filesInfoList.end())
+            {
+                if(iterFileInfo->suffix() == curSuffix)
+                {
+                    extensionsSizeList.last().second += iterFileInfo->size();
+                    filesInfoList.erase(iterFileInfo);
+                }
+                else
+                    iterFileInfo++;
+            }
+            extensionsSizeList.last().second /= dirSize;
+        }
+    }
+
+    return extensionsSizeList;
 }
 
 
