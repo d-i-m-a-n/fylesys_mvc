6c24dd66894f20e74266b2c647916aa481ba33d1
d-i-m-a <ivankin00@list.ru>
2021-05-31 00:00:14 +0700

    представление в виде столбиков

---
+++ barchart.cpp
@@ -0,0 +1,34 @@
+#include "barchart.h"
+
+#include "filesdatamodel.h"
+
+BarChart::BarChart(QWidget *parent) : MyView(parent)
+{
+    this->setAutoFillBackground(true);
+    this->setPalette(QPalette(QPalette::Background,Qt::white));
+}
+
+void BarChart::drawChart(QPainter &painter)
+{
+    int modelSize = model->rowCount(QModelIndex());
+    for(int i = 0; i < modelSize; i++)
+    {
+        //changeColor(painter, i % 12);
+        painter.setPen(palet[i%12]);
+        painter.setBrush(QBrush(palet[i%12],Qt::SolidPattern));
+        double percent = model->data(model->index(i,2),Qt::DisplayRole).toDouble();
+        painter.drawRect(drawRect.x() + 5 + 25*i, drawRect.height() - (drawRect.height() - 10) * percent / 100 ,
+                         25, (drawRect.height() - 10) * percent / 100);
+    }
+}
+
+void BarChart::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+
+    drawChart(painter);
+    printLegend(painter);
+
+}
+
---
+++ barchart.h
@@ -0,0 +1,24 @@
+#ifndef BARCHART_H
+#define BARCHART_H
+
+#include "myview.h"
+
+class BarChart : public MyView
+{
+    Q_OBJECT
+public:
+    BarChart(QWidget *parent);
+    virtual ~BarChart() = default;
+
+private:
+
+    void drawChart(QPainter &painter) override;
+
+    void paintEvent(QPaintEvent *event) override;
+
+
+
+
+};
+
+#endif // BARCHART_H
